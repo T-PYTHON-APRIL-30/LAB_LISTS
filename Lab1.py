6642634f24b3fcdbe4c5e0839cf104beb3995aea
Alaa <alaabinothman93@gmail.com>
2023-05-07 17:09:52 +0300

    lab1 with bonus علاء بن عثمان

--- Lab1.py
+++ Lab1.py
@@ -1,7 +1,8 @@
 # LAB_LISTS
 my_list=[2, 3, 4, 5, 15, 1, 43, 20]
 
-### Q1: Write a function that takes a list as a parameter, and then returns  the sum  of all the items in that list.
+### Q1: Write a function that takes a list as a parameter, 
+#and then returns  the sum  of all the items in that list.
 def addition(list):
     result=0
     for element in my_list:
@@ -10,7 +11,8 @@ def addition(list):
 
 addition(my_list)
 
-### Q2: Write a function that takes a list as a parameter, and then returns the largest number from a list of numbers.
+### Q2: Write a function that takes a list as a parameter, 
+#and then returns the largest number from a list of numbers.
 def largest(list):
     largest=0
     for element in my_list:
@@ -19,11 +21,31 @@ def largest(list):
     print(largest)
 largest(my_list)
 
-### Q3: Create an odd numbers list from the elements of a range from 1200 to 2000 with steps of 125, using [ list comprehension ].
-second_list=[*range(1200,2001)] #the * sympol unpacks the range function inside the list
-oddlist = [number for number in second_list[::125] if number%2 !=0]
+### Q3: Create an odd numbers list from the elements of a range 
+#from 1200 to 2000 with steps of 125, using [ list comprehension ].
+#second_list=[*range(1200,2001)] #the * sympol unpacks the range function inside the list
+oddlist = [number for number in range(1200,2000,125) if number%2 !=0]
 print(oddlist)
 
-### Q4: use list slicing to get a new list from the previous list starting from the start to the 5th element in the list.
+### Q4: use list slicing to get a new list from the previous 
+#list starting from the start to the 5th element in the list.
 fifth_element_list=oddlist[:4]
 print(fifth_element_list)
+
+#Bonus
+## Movie Ratings Analysis
+
+movies = [
+    ("The Shawshank Redemption", 1994, [9, 10, 10, 9, 8, 9]),
+    ("The Godfather", 1972, [10, 9, 8, 10, 9, 7]),
+    ("Pulp Fiction", 1994, [9, 8, 7, 8, 6, 5]),
+    ("The Dark Knight", 2008, [10, 9, 9, 8, 9, 8]),
+    ("Schindler's List", 1993, [8, 9, 9, 7, 6, 8]),
+    ("The Room", 2003, [1, 2, 3, 4, 5, 1])
+]
+for j in range(0,len(movies)):
+    i=[number for number in movies[j][2]]
+    avrage=round(sum(i)/len(i),2)
+    name,date=movies[j][0:2]
+    if avrage>6:
+        print(f"{j+1}. {name} ({date}) - Avergae rating: {avrage} *")
