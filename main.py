f0656bba3cedbfb479dbf68b9a15528a13c4ca3f
MK <MK.97@outlook.sa>
2023-05-08 14:05:13 +0300

    updated the lab

--- main.py
+++ main.py
@@ -23,11 +23,7 @@ print(f"maximum number in the list: {max(my_list)}")
 
 ### Q3: Create an odd numbers list from the elements of a range from 1200 to 2000 with steps of 125, using [ list comprehension ].
 
-# create the original list:
-original_list = list(range(1200,2000,125))
-print(f"the list of numbers from 1200 to 2000 with 125 steps: {original_list}")
-#new list with odd number
-odd_numbers_list = [number for number in original_list if number % 2 != 0]
+odd_numbers_list = [number for number in list(range(1200,2000,125)) if number % 2 != 0]
 print (f"the list of odd numbers: {odd_numbers_list}")
 
 ### Q4: use list slicing to get a new list from the previous list starting from the start to the 5th element in the list.
